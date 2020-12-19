8b9e50c5c97b578beb07ac0900974ad4cf55e89e
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-12-19 13:16:08 +0300

    create slider container

---
+++ src/components/slider/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+export default function Slider() {
+  return <SliderWrapper></SliderWrapper>
+}
+
+const SliderWrapper = styled.div`
+  height: 200px;
+  position: relative;
+  width: 50vw;
+  margin: 0 auto;
+  overflow: hidden;
+`
