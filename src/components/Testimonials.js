25a9a46dcce47f9d8bb661f2b47e2a2d111c6902
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-12-20 22:08:50 +0300

    add the Testimonial section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,40 +4,15 @@ import styled from "styled-components"
 import Section from "./page-elements/section"
 import Row from "./shared/Row"
 import SectionTitle from "./shared/SectionTitle"
+import Slides from "./slider/index"
 
 export default function Testimonials() {
   return (
     <Section>
       <Row>
         <SectionTitle title="Testimonials" />
-        <div class="testimonial-item testimonial-testimonials-5fdc48502728d">
-          <div class="testimonial-content">
-            <div class="testimonial-picture">
-              <img
-                src="//lmpixels.com/wp/leven-wp/wp-content/uploads/2019/12/testimonial-1.jpg"
-                alt="Billy Adams"
-              />
-            </div>
-            <div class="testimonial-text">
-              <p>
-                Donec eu est vel metus consequat volutpat. Nunc aliquet euismod
-                mauris, a feugiat urna ullamcorper non.
-              </p>
-            </div>
-
-            <div class="testimonial-author-info">
-              <h5 class="testimonial-author">Billy Adams</h5>
-              <p class="testimonial-firm">Rolling Thunder</p>
-            </div>
-
-            <div class="testimonial-icon">
-              <i class="fa fa-quote-left"></i>
-            </div>
-
-            <div class="testimonial-icon-big">
-              <i class="fa fa-quote-right"></i>
-            </div>
-          </div>
+        <div className="col-12">
+          <Slides />
         </div>
       </Row>
     </Section>
