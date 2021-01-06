3b6627c30fd979eb76acc6b2505463cfc2786292
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-06 21:22:05 +0300

    update the animation component

---
+++ src/components/animation/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Animate.propTypes = {
+  animation: PropTypes.string,
+  delay: PropTypes.string,
+  animationFunction: PropTypes.string,
+  duration: PropTypes.string,
+};
+
+Animate.defaultProps = {
+  animation: 'slide-up',
+  delay: '300',
+  animationFunction: 'ease',
+  duration: '800',
+};
+
+function Animate(props) {
+  const {
+    animation, delay, animationFunction, duration,
+  } = props;
+  return (
+    <div
+      data-sal={animation}
+      data-sal-delay={delay}
+      data-sal-easing={animationFunction}
+      data-sal-duration={duration}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Animate;
