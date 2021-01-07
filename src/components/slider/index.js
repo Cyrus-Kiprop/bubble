df2517dacb953a42cbcefc6e9aa49b125c1b248c
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-07 21:21:08 +0300

    fix issues with window not defined

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Arrow from './Arrow';
 
 import SlideContent from './SlideContent';
 
 export default function Slides({ children }) {
-  const getWidth = () => window.innerWidth;
-
-  const [state] = useState({
-    translate: 0,
-    transition: 0.45,
-  });
-
-  const { transition, translate } = state;
-
   return (
     <SliderWrapper>
-      <SlideContent
-        translate={translate}
-        transition={transition}
-        width={getWidth()}
-      >
-        {children}
-      </SlideContent>
+      <SlideContent>{children}</SlideContent>
 
       <Arrow />
     </SliderWrapper>
